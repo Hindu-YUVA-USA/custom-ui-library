cd0d9b0ae9b70ae7c89b49f37a4810d357852c31
krutartha <krutartha2002@gmail.com>
2024-07-08 17:19:27 -0400

    added comments and updated readme

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 This is the custom UI Library built in NextJS and TailwindCSS
 The documentation of this UI Library is supported by Storybook
 
-
 ## Getting Started
 
 Install Dependencies
 
-``` bash
+```bash
 npm i
 #or
 yarn add
 #or
 pnpm i #recommended
 ```
+
 Run the storybook server:
 
 ```bash
@@ -25,9 +25,11 @@ pnpm storybook
 
 Open [http://localhost:6006](http://localhost:6006) with your browser to see the result.
 
-You can start adding components by writing .tsx files in the `src/components/` directory. 
+You can start adding components by writing .tsx files in the `src/components/` directory.
 
-To enable storybook support for the newly written components, you will have to add a respective `.stories.tsx` file for your component in the `src/stories` directory.
+To enable storybook support for the newly written components, you will have to add a respective `.stories.tsx` file for your component.
+
+A simple example for a button has been done. Use that as a reference to add more components to the library!
 
 ## Learn More
 
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,6 +3,7 @@ import { cva, VariantProps } from "class-variance-authority";
 import { ComponentProps, forwardRef } from "react";
 
 const buttonStyles = cva(
+  //these are the default styles that will apply regardless of the chosen variant
   [
     "w-full",
     "rounded-md",
@@ -10,22 +11,27 @@ const buttonStyles = cva(
     "focus:outline-none",
     "disabled:cursor-not-allowed",
   ],
+  //below are the variants this component will support
   {
     variants: {
+      //one option/prop is a variant prop
       variant: {
         solid: "",
         outline: "border-2",
         ghost: "transition-colors duration-300",
       },
+      //another is a size prop
       size: {
         sm: "px-4 py-2 text-sm",
         md: "px-4 py-2 text-base",
         lg: "px-6 py-3 text-lg",
       },
+      //another is a coloscheme prop
       colorscheme: {
         primary: "text-white",
       },
     },
+    //styles to apply if sequence of props are chosen
     compoundVariants: [
       {
         variant: "solid",
@@ -44,6 +50,7 @@ const buttonStyles = cva(
         className: "text-primary-600 bg-transparent hover:bg-primary-100",
       },
     ],
+    //what the component should default to if no props are specified
     defaultVariants: {
       variant: "solid",
       size: "md",
@@ -52,8 +59,10 @@ const buttonStyles = cva(
   }
 );
 
+//definin the type of this component for type safety
 type ButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonStyles>;
 
+//using forwardRef to make this button callable when we import our libary in our projects
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ variant, size, colorscheme, className, ...props }, ref) => {
     return (
